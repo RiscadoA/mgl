dc094918f905c13e061069de45468a58d58ec8a5
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2018-10-09 20:56:23 +0100

    Improved tests

--- tests/math_quaternion_matrix4x4.c
+++ tests/math_quaternion_matrix4x4.c
@@ -11,14 +11,22 @@ int main(int argc, char** argv)
 	// Test mgl_f32q4_to_m4x4
 	{
 		mgl_f32q4_t q1;
-		mgl_f32q4_angle_axis(mgl_f32_deg_to_rad(45.0f), 0.0f, 1.0f, 0.0f, &q1);
-
 		mgl_f32m4x4_t m1;
-		mgl_f32m4x4_rotate_y(mgl_f32_deg_to_rad(45.0f), &m1);
-
 		mgl_f32m4x4_t m2;
-		mgl_f32q4_to_m4x4(&q1, &m2);
 
+		mgl_f32q4_angle_axis(mgl_f32_deg_to_rad(45.0f), 0.0f, 1.0f, 0.0f, &q1);
+		mgl_f32q4_to_m4x4(&q1, &m2);
+		mgl_f32m4x4_rotate_y(mgl_f32_deg_to_rad(45.0f), &m1);
+		TEST_REQUIRE_PASS(mgl_f32m4x4_equal_e(&m1, &m2, 0.001f));
+
+		mgl_f32q4_angle_axis(mgl_f32_deg_to_rad(45.0f), 1.0f, 0.0f, 0.0f, &q1);
+		mgl_f32q4_to_m4x4(&q1, &m2);
+		mgl_f32m4x4_rotate_x(mgl_f32_deg_to_rad(45.0f), &m1);
+		TEST_REQUIRE_PASS(mgl_f32m4x4_equal_e(&m1, &m2, 0.001f));
+
+		mgl_f32q4_angle_axis(mgl_f32_deg_to_rad(45.0f), 0.0f, 0.0f, 1.0f, &q1);
+		mgl_f32q4_to_m4x4(&q1, &m2);
+		mgl_f32m4x4_rotate_z(mgl_f32_deg_to_rad(45.0f), &m1);
 		TEST_REQUIRE_PASS(mgl_f32m4x4_equal_e(&m1, &m2, 0.001f));
 	}
 
