af11478c01b4c5471f9bff8269942b9ef5a2860d
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2018-11-01 20:27:06 +0000

    Added iterator

---
+++ src/mgl/container/iterator.c
@@ -0,0 +1,49 @@
+#include <mgl/container/iterator.h>
+
+mgl_error_t mgl_iterator_next(const mgl_iterator_t * it, mgl_iterator_t * out)
+{
+	MGL_DEBUG_ASSERT(it != NULL && out != NULL);
+	if (it->functions->next == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->next(it, out);
+}
+
+mgl_error_t mgl_iterator_prev(const mgl_iterator_t * it, mgl_iterator_t * out)
+{
+	MGL_DEBUG_ASSERT(it != NULL && out != NULL);
+	if (it->functions->prev == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->prev(it, out);
+}
+
+mgl_error_t mgl_iterator_move(const mgl_iterator_t * it, mgl_iterator_t * out, mgl_i64_t move)
+{
+	MGL_DEBUG_ASSERT(it != NULL && out != NULL);
+	if (it->functions->move == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->move(it, out, move);
+}
+
+mgl_bool_t mgl_iterator_is_null(const mgl_iterator_t * it)
+{
+	MGL_DEBUG_ASSERT(it != NULL);
+	if (it->functions->is_null == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->is_null(it);
+}
+
+void * mgl_iterator_get(const mgl_iterator_t * it)
+{
+	MGL_DEBUG_ASSERT(it != NULL);
+	if (it->functions->get == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->get(it);
+}
+
+void * mgl_iterator_get_raw(const mgl_iterator_t * it)
+{
+	MGL_DEBUG_ASSERT(it != NULL);
+	if (it->functions->get_raw == NULL)
+		return MGL_ERROR_UNSUPPORTED;
+	return it->functions->get_raw(it);
+}
