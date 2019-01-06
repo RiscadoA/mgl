c797bc438a0d2f3e0405bee9c28c038d2955c3c1
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2019-01-06 17:06:14 +0000

    Changed splines

--- include/mgl/math/bezier_curve.h
+++ include/mgl/math/bezier_curve.h
@@ -8,19 +8,31 @@
 extern "C" {
 #endif // __cplusplus
 
+	typedef struct
+	{
+		mgl_point_2d_t p0;
+		mgl_point_2d_t p1;
+		mgl_point_2d_t p2;
+	} mgl_bq_spline_t;
+
 	/// <summary>
 	///		Calculates a point in a quad bezier curve.
 	/// </summary>
-	/// <param name="p0">First point</param>
-	/// <param name="p1">Second point</param>
-	/// <param name="p2">Third point</param>
+	/// <param name="spline">Spline</param>
 	/// <param name="t">Interpolating value (from 0 to 1)</param>
 	/// <param name="out">Out point</param>
-	void MGL_API mgl_bezier_quad_2d(const mgl_point_2d_t* p0,
-									const mgl_point_2d_t* p1,
-									const mgl_point_2d_t* p2, 
-									mgl_f32_t t,
-									mgl_point_2d_t* out);
+	void MGL_API mgl_bezier_quad_2d_point(const mgl_bq_spline_t* spline,
+										  mgl_f32_t t,
+										  mgl_point_2d_t* out);
+
+	/// <summary>
+	///		Calculates the distance of a point to a quad bezier curve.
+	/// </summary>
+	/// <param name="spline">Spline</param>
+	/// <param name="p">Point</param>
+	/// <returns>Distance squared</returns>
+	mgl_f32_t MGL_API mgl_bezier_quad_2d_dist(const mgl_bq_spline_t* spline,
+											  const mgl_point_2d_t* p);
 
 #ifdef __cplusplus
 }
--- src/mgl/math/bezier_curve.c
+++ src/mgl/math/bezier_curve.c
@@ -1,17 +1,20 @@
 #include <mgl/math/bezier_curve.h>
 #include <mgl/math/scalar.h>
 
-void MGL_API mgl_bezier_quad_2d(const mgl_point_2d_t* p0,
-								const mgl_point_2d_t* p1,
-								const mgl_point_2d_t* p2,
-								mgl_f32_t t,
-								mgl_point_2d_t* out)
+void MGL_API mgl_bezier_quad_2d_point(const mgl_bq_spline_t* spline,
+									  mgl_f32_t t,
+									  mgl_point_2d_t* out)
 {
 	mgl_point_2d_t a, b;
-	a.x = mgl_f32_lerp(p0->x, p1->x, t);
-	a.y = mgl_f32_lerp(p0->y, p1->y, t);
-	b.x = mgl_f32_lerp(p1->x, p2->x, t);
-	b.y = mgl_f32_lerp(p1->y, p2->y, t);
+	a.x = mgl_f32_lerp(spline->p0.x, spline->p1.x, t);
+	a.y = mgl_f32_lerp(spline->p0.y, spline->p1.y, t);
+	b.x = mgl_f32_lerp(spline->p1.x, spline->p2.x, t);
+	b.y = mgl_f32_lerp(spline->p1.y, spline->p2.y, t);
 	out->x = mgl_f32_lerp(a.x, b.x, t);
 	out->y = mgl_f32_lerp(a.y, b.y, t);
+}
+
+mgl_f32_t MGL_API mgl_bezier_quad_2d_dist(const mgl_bq_spline_t * spline, const mgl_point_2d_t * p)
+{
+	return 0.0f;
 }
