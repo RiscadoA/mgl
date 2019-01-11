7bc60ae9a94039deccca3915d26d630f13820e40
Ricardo Antunes <32821840+RiscadoA@users.noreply.github.com>
2019-01-11 15:48:24 +0000

    Added a logger

---
+++ include/mgl/file/logger.h
@@ -0,0 +1,36 @@
+#ifndef MGL_FILE_LOGGER_H
+#define MGL_FILE_LOGGER_H
+
+#include <mgl/type.h>
+#include <mgl/api_utils.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+	typedef struct mgl_logger_t mgl_logger_t;
+
+	/// <summary>
+	///		Opens a logger.
+	/// </summary>
+	/// <param name="allocator">Allocator pointer</param>
+	/// <returns>Pointer to logger</returns>
+	mgl_logger_t* MGL_API mgl_logger_open(void* allocator);
+
+	/// <summary>
+	///		Outputs the log file.
+	/// </summary>
+	/// <param name="logger">Logger</param>
+	/// <param name="text">Text</param>
+	void MGL_API mgl_logger_add(mgl_logger_t* logger, const mgl_chr8_t* text);
+
+	/// <summary>
+	///		Closes a logger.
+	/// </summary>
+	/// <param name="logger">Logger</param>
+	void MGL_API mgl_logger_close(mgl_logger_t* logger);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // MGL_FILE_LOGGER_H
---
+++ src/mgl/file/logger.c
@@ -0,0 +1,59 @@
+#include <mgl/file/logger.h>
+#include <mgl/memory/allocator.h>
+#include <mgl/stream/buffer_stream.h>
+
+#include <stdio.h>
+#include <time.h>
+
+#define MGL_LOGGER_BUFFER_SIZE 4096
+
+struct mgl_logger_t
+{
+	void* allocator;
+	mgl_chr8_t buffer[MGL_LOGGER_BUFFER_SIZE];
+	mgl_u64_t size;
+	FILE* file;
+};
+
+mgl_logger_t *MGL_API mgl_logger_open(void * allocator)
+{
+	MGL_DEBUG_ASSERT(allocator != NULL);
+	mgl_logger_t* logger;
+
+	mgl_chr8_t file_name[256] = { 0 };
+
+	{
+		mgl_buffer_stream_t stream;
+		mgl_init_buffer_stream(&stream, file_name, sizeof(file_name));
+
+		time_t tm;
+		time(&tm);
+		mgl_print(&stream, u8"log_");
+		mgl_print_u64(&stream, tm, 10);
+		mgl_print(&stream, u8".txt");
+	}
+
+	FILE* file = fopen(file_name, "w");
+
+	MGL_DEBUG_ASSERT(file != NULL);
+
+	MGL_DEBUG_ASSERT(mgl_allocate(allocator, sizeof(mgl_logger_t), (void**)&logger) == MGL_ERROR_NONE);
+	logger->allocator = allocator;
+	logger->size = 0;
+	logger->file = file;
+
+	return logger;
+}
+
+void MGL_API mgl_logger_add(mgl_logger_t * logger, const mgl_chr8_t * text)
+{
+	MGL_DEBUG_ASSERT(logger != NULL && text != NULL);
+	fputs(text, logger->file);
+}
+
+void MGL_API mgl_logger_close(mgl_logger_t * logger)
+{
+	MGL_DEBUG_ASSERT(logger != NULL);
+	fclose(logger->file);
+	MGL_DEBUG_ASSERT(mgl_deallocate(logger->allocator, logger) == MGL_ERROR_NONE);
+}
